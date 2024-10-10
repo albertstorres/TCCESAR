7c2ac9fa32c6715dbe3bd170e4b113b929bdfc17
Albert Sevy <albertstorres@gmail.com>
2024-10-10 07:06:54 -0300

    Início de desenvolvimento do projeto TCCESAR

---
+++ src/main/java/com/cesar/tccesar/TccesarApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.tccesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccesarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccesarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar/tccesar/TccesarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.tccesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccesarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
